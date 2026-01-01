23d1e10a4210717fc5183e81add5307c1b4e716a
bogier <111124301+bogier@users.noreply.github.com>
2026-01-01 22:01:18 +0100

    version notification

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Mode OFFLINE FULL - App Shell / Cache-first
    
 */
-const STATIC_CACHE = `TidyZou-static-2025-12-31-2030`;
+const STATIC_CACHE = `TidyZou-static-2026-01-01-2158`;
 
 
 // 🔧 À adapter si tu changes les versions de tes assets (v=3.6, v=3.7, etc.)
