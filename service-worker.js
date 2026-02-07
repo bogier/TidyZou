bb22fa869650df609f30f99d43bbcb3939503203
bogier <111124301+bogier@users.noreply.github.com>
2026-02-07 20:30:58 +0100

    version tri des taches

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Mode OFFLINE FULL - App Shell / Cache-first
    
 */
-const STATIC_CACHE = `TidyZou-static-2026-02-07-0251`;
+const STATIC_CACHE = `TidyZou-static-2026-02-07-2006`;
 
 
 // 🔧 À adapter si tu changes les versions de tes assets (v=3.6, v=3.7, etc.)
