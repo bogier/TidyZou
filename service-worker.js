bfa6f6c885d29658ac5b582f2b1d7f7366cc804c
bogier <111124301+bogier@users.noreply.github.com>
2026-02-12 19:59:04 +0100

    version avec QrCode

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Mode OFFLINE FULL - App Shell / Cache-first
    
 */
-const STATIC_CACHE = `TidyZou-static-2026-02-07-2051`;
+const STATIC_CACHE = `TidyZou-static-2026-02-12-1951`;
 
 
 // 🔧 À adapter si tu changes les versions de tes assets (v=3.6, v=3.7, etc.)
